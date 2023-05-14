6edd65075529e920cd8c112f82b11b796a4b8741
MarVasilieva <marina_vasilieva@list.ru>
2023-05-14 22:41:52 +0600

    add count of words and focus

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Wordcard.css";
 import "./styles.scss";
 import "../App.css";
@@ -6,79 +6,68 @@ import Wordcard from "./Wordcard";
 import "./Wordlist";
 import words from "./words.json";
 
-class Cards extends React.Component {
-  state = {
-    words: words,
-    Index: 0,
-  };
+const Cards = () => {
+  const [index, setIndex] = useState(0);
+  const [isPrev, setIsPrev] = useState(false);
 
-  handlePrev = () => {
-    this.setState((prevState) => ({
-      Index:
-        prevState.Index === 0
-          ? prevState.words.length - 1
-          : prevState.Index - 1,
-      isPrev: true,
-    }));
-  };
-
-  handleNext = () => {
-    this.setState((prevState) => ({
-      Index:
-        prevState.Index === prevState.words.length - 1
-          ? 0
-          : prevState.Index + 1,
-      isPrev: false,
-    }));
-  };
-
-  render() {
-    const Word2 =
-      this.state.Index + 1 >= this.state.words.length
-        ? this.state.words[0]
-        : this.state.words[this.state.Index + 1];
-    const Word3 =
-      this.state.Index + 2 >= this.state.words.length
-        ? this.state.words[1]
-        : this.state.words[this.state.Index + 2];
-    const Word = this.state.words[this.state.Index];
-    return (
-      <div className="wordsCards">
-        <div>
-          {" "}
-          <button className="btn" onClick={this.handlePrev}>
-            Предыдущая
-          </button>
-        </div>
-        <Wordcard
-          title={Word.title}
-          transcription={Word.transcription}
-          translation={Word.translation}
-          url={Word.url}
-          isPrev={this.state.isPrev}
-        ></Wordcard>
-        <Wordcard
-          title={Word2.title}
-          transcription={Word2.transcription}
-          translation={Word2.translation}
-          url={Word2.url}
-          isPrev={this.state.isPrev}
-        ></Wordcard>
-        <Wordcard
-          title={Word3.title}
-          transcription={Word3.transcription}
-          translation={Word3.translation}
-          url={Word3.url}
-          isPrev={this.state.isPrev}
-        ></Wordcard>
-        <div>
-          <button className="btn" onClick={this.handleNext}>
-            Следующая
-          </button>
-        </div>
-        {this.state.Index + 1}/{words.length}
-      </div>
+  const handlePrev = () => {
+    setIndex((prevIndex) =>
+      prevIndex === 0 ? words.length - 1 : prevIndex - 1
     );
-  }
-}
+    setIsPrev(true);
+  };
+
+  const handleNext = () => {
+    setIndex((prevIndex) =>
+      prevIndex === words.length - 1 ? 0 : prevIndex + 1
+    );
+    setIsPrev(false);
+  };
+
+  const getWord = (currentIndex) => {
+    return words[currentIndex];
+  };
+
+  const Word = getWord(index);
+  const Word2 = getWord(index + 1 >= words.length ? 0 : index + 1);
+  const Word3 = getWord(index + 2 >= words.length ? 1 : index + 2);
+
+  return (
+    <div className="wordsCards">
+      <div>
+        <button className="btn" onClick={handlePrev}>
+          Предыдущая
+        </button>
+      </div>
+      <Wordcard
+        title={Word.title}
+        transcription={Word.transcription}
+        translation={Word.translation}
+        url={Word.url}
+        isPrev={isPrev}
+      />
+      <Wordcard
+        title={Word2.title}
+        transcription={Word2.transcription}
+        translation={Word2.translation}
+        url={Word2.url}
+        isPrev={isPrev}
+      />
+      <Wordcard
+        title={Word3.title}
+        transcription={Word3.transcription}
+        translation={Word3.translation}
+        url={Word3.url}
+        isPrev={isPrev}
+      />
+      <div>
+        <button className="btn" onClick={handleNext}>
+          Следующая
+        </button>
+      </div>
+      {index + 1}/{words.length}
+    </div>
+  );
+};
+
 export default Cards;
--- src/components/Wordcard.jsx
+++ src/components/Wordcard.jsx
@@ -1,10 +1,26 @@
 import "./Wordcard.css";
 import "./styles.scss";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function Wordcard(props) {
   const [isShowButton, setButton] = useState(true);
+  const [count, setCount] = useState(0);
+
   useEffect(() => setButton(true), [props.translation]);
+
+  const handleButtonClick = () => {
+    setButton(!isShowButton);
+    setCount(count + 1);
+  };
+
+  const buttonRef = useRef(null);
+
+  useEffect(() => {
+    if (isShowButton) {
+      buttonRef.current.focus();
+    }
+  }, [isShowButton]);
+
   return (
     <div className="word">
       <div className="wordTitle">
@@ -15,16 +31,18 @@ function Wordcard(props) {
       </div>
       <div className="wordButton">
         {isShowButton ? (
-          <button className="btn" onClick={() => setButton(!isShowButton)}>
+          <button className="btn" onClick={handleButtonClick} ref={buttonRef}>
             Проверить
           </button>
         ) : (
-          <h3 onClick={() => setButton(!isShowButton)}>
-            <h3> {props.translation}</h3> <img src={props.url}></img>
+          <h3 onClick={handleButtonClick}>
+            <h3>{props.translation}</h3> <img src={props.url} alt="" />
           </h3>
         )}
       </div>
+      <div className="counter">Счетчик: {count}</div>
     </div>
   );
 }
+
 export default Wordcard;
